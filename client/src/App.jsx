d34ccf0d25e8a6e66e51d9d2897d7cea2cce8cf7
Raul Birsan <raul.birsan@endava.com>
2025-08-08 14:34:38 +0300

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <div style={{ padding: 32 }}>
+      <h1>React + MUI Client (Vite)</h1>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+    </div>
+  );
+}
+
+export default App;
